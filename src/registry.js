163a465471c867e3eda647cdb23e438f553bdc12
KantariaDhaval <dkantaria@tekion.com>
2021-04-06 12:14:01 +0530

    Removed unnecessery files

--- src/registry.js
+++ src/registry.js
@@ -5,11 +5,4 @@ const tabNames = {
   TAGGED: "TAGGED",
 };
 
-const actionTypes = {
-  SET_CURRENT_TAB: "SET_CURRENT_TAB",
-  SET_LOADING: "SET_LOADING",
-  SET_ERROR: "SET_ERROR",
-  SET_DATA: "SET_DATA",
-};
-
-export { tabNames, actionTypes };
+export { tabNames };
