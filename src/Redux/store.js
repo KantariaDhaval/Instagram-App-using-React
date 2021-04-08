89f1967b803baa621540f892ef253fba8680615f
KantariaDhaval <dkantaria@tekion.com>
2021-04-08 10:06:44 +0530

    checking commands

--- src/Redux/store.js
+++ src/Redux/store.js
@@ -2,15 +2,9 @@ import { configureStore } from "@reduxjs/toolkit";
 import rootReducer from "./Reducers";
 import logger from "redux-logger";
 
-const myLogger = (store) => (next) => (action) => {
-  console.log("Logged actions: ", action);
-  next(action);
-};
-
 const store = configureStore({
   reducer: rootReducer,
-  middleware: (getDefaultMiddleware) =>
-    getDefaultMiddleware().concat(myLogger, logger),
+  middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(logger),
 });
 
 store.subscribe(() => {
